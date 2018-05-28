4bae0b74d09fe0776c91850dd2ce29384361a786
Choi From iMac <shchoi@penandfree.com>
2018-05-28 09:51:53 +0900

    Copyright 수정

--- Common/Common/Toast+UIView.m
+++ Common/Common/Toast+UIView.m
@@ -1,9 +1,9 @@
 //
 //  Toast+UIView.m
+//  PenTestExtension
 //
-//
-//
-//  Copyright 2011
+//  Created by Luidia on 2018. 05. 04..
+//  Copyright © 2018년 Luidia. All rights reserved.
 //
 
 #import "Toast+UIView.h"
--- Common/PNFStrokePoint/PNFStrokePoint.h
+++ Common/PNFStrokePoint/PNFStrokePoint.h
@@ -1,6 +1,6 @@
 //
 //  PNFStrokePoint.h
-//  MINTInteractive
+//  PenTestExtension
 //
 //  Created by Luidia on 2018. 05. 04..
 //  Copyright © 2018년 Luidia. All rights reserved.
